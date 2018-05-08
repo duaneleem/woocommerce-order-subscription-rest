ea48f95f92a5f707ce43db3a973572370e404e76
Ubuntu <ubuntu@ip-172-31-28-4.us-west-2.compute.internal>
2018-05-08 23:36:02 +0000

    Fixed but that only parent ID of subscription worked.

--- inc/class.ep-customization.php
+++ inc/class.ep-customization.php
@@ -5,23 +5,22 @@ class EPC_Class {
         add_filter( 'woocommerce_api_order_response', array($this, 'EPC_wc_api_order_response'), 10, 1 ); 
     }
 
-    public function EPC_wc_api_order_response( $order_data ) {
-         $subscriptions_ids = wcs_get_subscriptions_for_order( $order_data['id'] );
-        // We get the related subscription for this order
-        foreach( $subscriptions_ids as $subscription_id => $subscription_obj ){
-            
-                $order_data['subscription_id'] = $subscription_id;
+  public function EPC_wc_api_order_response( $order_data ) {
+    $subscriptions_ids = wcs_get_subscriptions_for_order( $order_data["id"], array('order_type' => 'any'));
+    
+    // Get the related subscription for this order
+    foreach( $subscriptions_ids as $subscription_id => $subscription_obj ){
+      $order_data['subscription_id'] = $subscription_id;
 
+      $subscriptions = $subscription_obj->get_related_orders();
 
-                $subscriptions = $subscription_obj->get_related_orders();
+      $order_data["no_of_orders"] = sizeof($subscriptions);
+      
+      return $order_data;
+    } // foreach()
 
-                $order_data['no_of_orders'] = sizeof($subscriptions);
-                return $order_data;
-           
-        }
-
-        $order_data['subscription_id'] = "";
-        $order_data['no_of_subscriptions']  = 0;
-        return $order_data;
-    }
+    $order_data["subscription_id"] = "";
+    $order_data["no_of_subscriptions"]  = 0;
+    return $order_data;
+  }
 }
